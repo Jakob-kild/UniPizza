7e7e6b68561713990a8a1f081d7cf2c23b947ea7
Jakob Kildegaard <jakobkilde@gmail.com>
2022-06-16 11:54:38 +0200

    database

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-
+import 'package:cloud_firestore_platform_interface/cloud_firestore_platform_interface.dart';
 import '../classes/Pizzaria.dart';
 
 class Home extends StatefulWidget {
--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:world_time_app/classes/Pizzaria.dart';
+import 'package:cloud_firestore/cloud_firestore.dart'
 
 class Loading extends StatefulWidget {
   @override
@@ -19,6 +20,10 @@ class _LoadingState extends State<Loading> {
       'isDaytime': instance.isDaytime
     });
   }*/
+  Future<void> init() async {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
 
 
 
