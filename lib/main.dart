462984604f0e5f5d06efa23322862c26d6898b4d
VicAceCase <victor.larsen5@hotmail.com>
2022-06-20 21:22:48 +0200

    launch icon, rotation lock made

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,13 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:uni_pizzas/pages/homeScreen.dart';
 import 'package:uni_pizzas/pages/restaurant_details.dart';
+import 'package:flutter/services.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp,DeviceOrientation.portraitDown]);
   await Firebase.initializeApp();
-  runApp(MaterialApp(theme: theme(), initialRoute: '/', routes: {
+  runApp(MaterialApp(debugShowCheckedModeBanner: false,theme: theme(), initialRoute: '/', routes: {
     '/': (context) => HomeScreen(),
     '/home': (context) => RestaurantDetailsScreen(),
     '/map' : (context) => MapScreen(),
