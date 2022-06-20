6a2cc359c1b549dc255ee3e6ec5482de3761c348
SovereignPihl <marcuspihl01@gmail.com>
2022-06-20 12:39:58 +0200

    Changed into actual navbar

--- lib/pages/homeScreen.dart
+++ lib/pages/homeScreen.dart
@@ -9,65 +9,53 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int pageIndex = 0;
+  int _selectedIndex = 0;
 
   @override
   void initState() {
     super.initState();
   }
-
   final pages = [
-    menuScreen(),
     map(),
     search(),
+    menuScreen(),
   ];
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(),
-      body: pages[pageIndex],
-      bottomNavigationBar: CustomNavigationBar(context),
+      body: pages.elementAt(_selectedIndex),
+      bottomNavigationBar: CustomNavigationBar(),
     );
   }
 
-
-  Container CustomNavigationBar(BuildContext context) {
-    return Container(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          IconButton(
-              onPressed: () {
-                setState(() {
-                  pageIndex = 1;
-                });
-              },
-              icon: const Icon(Icons.location_on)
-          ),
-          IconButton(
-            onPressed: () {
-              setState(() {
-                pageIndex = 0;
-              });
-
-            },
-            icon: const Icon(Icons.home),
-          ),
-          IconButton(
-            onPressed: () {
-              setState(() {
-                pageIndex = 2;
-              });
-            },
-            icon: const Icon(Icons.search),
-          ),
-        ],
-      ),
+  BottomNavigationBar CustomNavigationBar() {
+    return BottomNavigationBar(
+      currentIndex: _selectedIndex,
+      selectedItemColor: Colors.amber[800],
+      onTap: _onItemTapped,
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.location_on),
+          label: 'Map'
+        ),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home'
+        ),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search'
+        )
+      ],
     );
   }
-
+  void _onItemTapped(int index){
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 }
 
 class RestaurantCard extends StatelessWidget {
@@ -88,10 +76,7 @@ class RestaurantCard extends StatelessWidget {
           children: [
             Stack(children: [
               Container(
-                  width: MediaQuery
-                      .of(context)
-                      .size
-                      .width,
+                  width: MediaQuery.of(context).size.width,
                   height: 150,
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(5.0),
@@ -113,10 +98,7 @@ class RestaurantCard extends StatelessWidget {
                       alignment: Alignment.center,
                       child: Text(
                         '${restaurant.deliveryTime} min',
-                        style: Theme
-                            .of(context)
-                            .textTheme
-                            .bodyText1,
+                        style: Theme.of(context).textTheme.bodyText1,
                       ),
                     )),
               )
@@ -127,41 +109,27 @@ class RestaurantCard extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(restaurant.name,
-                      style: Theme
-                          .of(context)
-                          .textTheme
-                          .headline5),
+                      style: Theme.of(context).textTheme.headline5),
                   SizedBox(height: 5),
 
                   // Text('${restaurant.tags}'),
                   Row(
                       children: restaurant.tags
                           .map(
-                            (tag) =>
-                        restaurant.tags.indexOf(tag) ==
-                            restaurant.tags.length - 1
-                            ? Text(tag,
-                            style:
-                            Theme
-                                .of(context)
-                                .textTheme
-                                .bodyText1)
-                            : Text('$tag, ',
-                            style:
-                            Theme
-                                .of(context)
-                                .textTheme
-                                .bodyText1),
-                      )
+                            (tag) => restaurant.tags.indexOf(tag) ==
+                                    restaurant.tags.length - 1
+                                ? Text(tag,
+                                    style:
+                                        Theme.of(context).textTheme.bodyText1)
+                                : Text('$tag, ',
+                                    style:
+                                        Theme.of(context).textTheme.bodyText1),
+                          )
                           .toList()),
                   SizedBox(height: 5),
                   Text(
-                      '${restaurant.distance}km -\$${restaurant
-                          .deliveryFee} delivery fee',
-                      style: Theme
-                          .of(context)
-                          .textTheme
-                          .bodyText1)
+                      '${restaurant.distance}km -\$${restaurant.deliveryFee} delivery fee',
+                      style: Theme.of(context).textTheme.bodyText1)
                 ],
               ),
             ),
@@ -187,14 +155,11 @@ class CustomAppBar extends StatelessWidget with PreferredSizeWidget {
           ),
           centerTitle: false,
           title:
-          Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
             Text(
               'CURRENT LOCATION',
               style:
-              DefaultTextStyle
-                  .of(context)
-                  .style
-                  .apply(fontSizeFactor: 1.0),
+                  DefaultTextStyle.of(context).style.apply(fontSizeFactor: 1.0),
             ),
             Text('Virum, Danmark')
           ])),
@@ -207,7 +172,6 @@ class CustomAppBar extends StatelessWidget with PreferredSizeWidget {
 }
 
 class menuScreen extends StatefulWidget {
-
   @override
   State<menuScreen> createState() => _menuScreenState();
 }
@@ -218,32 +182,27 @@ class _menuScreenState extends State<menuScreen> {
     return Container(
       child: SingleChildScrollView(
           child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Align(
-                    alignment: Alignment.topLeft,
-                    child: Text(
-                      'Pizzeriaer',
-                      style: Theme
-                          .of(context)
-                          .textTheme
-                          .headline4,
-                    )),
-              ),
-              ListView.builder(
-                shrinkWrap: true,
-                physics: NeverScrollableScrollPhysics(),
-                itemCount: Restaurant.restaurants.length,
-                itemBuilder: (context, index) {
-                  return RestaurantCard(
-                      restaurant: Restaurant.restaurants[index]);
-                },
-              ),
-            ],
-          )),
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  'Pizzeriaer',
+                  style: Theme.of(context).textTheme.headline4,
+                )),
+          ),
+          ListView.builder(
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(),
+            itemCount: Restaurant.restaurants.length,
+            itemBuilder: (context, index) {
+              return RestaurantCard(restaurant: Restaurant.restaurants[index]);
+            },
+          ),
+        ],
+      )),
     );
   }
 }
-
